1366955d696187ceef20dab7d588490aa6c88808
Nina Alhimovich of 150502 <alhimovich.nina@gmail.com>
2024-11-20 19:51:32 +0300

    Prepare for production

--- app.js
+++ app.js
@@ -32,12 +32,16 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/jobOpenings', jobOpeningsRouter);
 
-// for the React-app
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+// Обслуживание статики
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-// for other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 // catch 404 and forward to error handler
