2bb668e8e43d4af8e07b2800afb2722f55d22017
Nina Alhimovich of 150502 <alhimovich.nina@gmail.com>
2024-11-20 11:38:23 +0300

    Implement design

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VacanciesTable from "./components/VacanciesTable";
+
+function App() {
+  return (
+      <div className="App">
+        <h1>Job Openings</h1>
+        <VacanciesTable/>
+      </div>
+  );
+}
+
+export default App;
